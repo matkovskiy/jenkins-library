fcf61f770a1aed5cba20725278506521fa84ea3c
Matkovskiy Aleksandr <amatkovskiy>
2019-12-09 15:36:11 +0200

    added git_merge and git_pull

--- src/com/amatkovskiy/ci/commands.groovy
+++ src/com/amatkovskiy/ci/commands.groovy
@@ -24,3 +24,51 @@ def git_pull_singl_branch (git_repository, git_branch, credentialsId, directory=
     }
   }
 }
+
+
+def git_pull (git_repository, credentialsId, directory=''){
+  withCredentials([sshUserPrivateKey(credentialsId: credentialsId, keyFileVariable: 'SSH_KEY', passphraseVariable: 'SSH_PASS', usernameVariable: 'SSH_USER')]) {
+    try {
+      echo "git_branch = " + git_branch
+      sh """
+      set +x
+        eval `ssh-agent -a ~/.ssh-agent.sock`
+        ( openssl rsa -passin env:SSH_PASS -in ${SSH_KEY} | ssh-add -  ) || ( ssh-agent -k && exit 1)
+        git clone ${git_repository} ${directory}
+        ssh-agent -k
+      """
+    }
+    catch (Exception e) {
+      def command = 'ps ax | grep "/var/lib/jenkins/.ssh-agent.soc[k]" | awk \'{print $1}\'  | xargs -n1 -I{} bash -c "SSH_AGENT_PID={} ssh-agent -a  ~/.ssh-agent.sock -k"'
+      echo command
+      sh(returnStdout: true, script: command).trim()
+      error 'Failed to pull repository'
+    }
+  }
+}
+
+
+def git_merge (git_branch, src_branch, dst_branch, commit_message, credentialsId, directory=''){
+  withCredentials([sshUserPrivateKey(credentialsId: credentialsId, keyFileVariable: 'SSH_KEY', passphraseVariable: 'SSH_PASS', usernameVariable: 'SSH_USER')]) {
+    try {
+      sh """
+      set +x
+        eval `ssh-agent -a ~/.ssh-agent.sock`
+        ( openssl rsa -passin env:SSH_PASS -in ${SSH_KEY} | ssh-add -  ) || ( ssh-agent -k && exit 1)
+        git clone ${git_repository} ${directory}
+        cd ${directory}
+        git chechout ${dst_branch}
+        git merge ${src_branch} -m "${commit_message}"
+        
+        git push
+        ssh-agent -k
+      """
+    }
+    catch (Exception e) {
+      def command = 'ps ax | grep "/var/lib/jenkins/.ssh-agent.soc[k]" | awk \'{print $1}\'  | xargs -n1 -I{} bash -c "SSH_AGENT_PID={} ssh-agent -a  ~/.ssh-agent.sock -k"'
+      echo command
+      sh(returnStdout: true, script: command).trim()
+      error 'Failed to pull repository'
+    }
+  }
+}
