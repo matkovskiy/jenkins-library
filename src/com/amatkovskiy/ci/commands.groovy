344b2e3344003bc1c180cd5f6f3cb0f29eb4abf7
Matkovskiy Aleksandr <amatkovskiy>
2020-04-03 21:06:20 +0300

    2131

--- src/com/amatkovskiy/ci/commands.groovy
+++ src/com/amatkovskiy/ci/commands.groovy
@@ -11,8 +11,7 @@ def add_host_to_known_hosts (hostname) {
 
       mkdir -p ~/.ssh/
       hostname_clear=`echo "${hostname}" | cut -d "@" -f2 | cut -d ":" -f1`
-      echo "hostname_clear = ${hostname_clear}"
-      
+      ssh-keyscan -H ${hostname_clear} >> ~/.ssh/known_hosts
     """
   }
   catch (Exception e) {
